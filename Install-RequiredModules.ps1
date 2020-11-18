dd1e30c6b1d48dc070c4fe2ec49ba841158de8d8
Jan Řežab <rezabj@users.noreply.github.com>
2020-11-18 12:59:01 +0100

    [ADD] Install-RequiredModules.ps1

---
+++ Install-RequiredModules.ps1
@@ -0,0 +1,5 @@
+Install-Module -Name logging
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+Install-Module -Name AzureAD
+Install-Module -Name MSOnline
+Install-Module -Name ImportExcel
